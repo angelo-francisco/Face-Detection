b221c7a3bc6797e42212d05580e839925a3d55fc
angelo-francisco <ics20080729@gmail.com>
2024-07-22 13:53:49 +0100

    face detection done

---
+++ src/main.py
@@ -0,0 +1,28 @@
+import cv2
+import mediapipe.python.solutions.face_detection as fd
+import mediapipe.python.solutions.drawing_utils as drw
+
+
+webcam = cv2.VideoCapture(0)
+face_detector = fd.FaceDetection()
+draw = drw
+
+
+while True:
+    verify, frame = webcam.read()
+
+    if not verify:
+        break
+    
+    rosts = face_detector.process(frame)
+
+    if rosts.detections:
+        for rost in rosts.detections:
+            draw.draw_detection(frame, rost)
+
+    cv2.imshow('GG', frame)
+    if cv2.waitKey(5) == 27:
+        break
+
+webcam.release()
+cv2.destroyAllWindows()
